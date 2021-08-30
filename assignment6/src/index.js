2e030f1611ab7c888472f2365c55751503e3f7de
kjm541006 <kjm541113@gmail.com>
2021-08-30 18:43:01 +0900

    finish

--- assignment6/src/index.js
+++ assignment6/src/index.js
@@ -1,16 +1,18 @@
 const randomNumInput = document.querySelector("h2 input");
 const guessNumInput = document.querySelector("h3 input");
 const form = document.querySelector("#form");
+const div = document.querySelector("div");
+
+const h3 = document.createElement("h3");
+const h2 = document.createElement("h2");
+div.appendChild(h3);
+div.appendChild(h2);
 
 function createRandomNum(randomNumInput) {
   randomNum = Math.floor(Math.random() * randomNumInput + 1);
 }
 
 function checkRandomNum() {
-  const h3 = document.createElement("h3");
-  const h2 = document.createElement("h2");
-  form.appendChild(h3);
-  form.appendChild(h2);
   if (Number(guessNumInput.value) === randomNum) {
     h3.innerText = `You choose: ${guessNumInput.value}, the machine chose: ${randomNum}`;
     h2.innerText = "You Won!";
